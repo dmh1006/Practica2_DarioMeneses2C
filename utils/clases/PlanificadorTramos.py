60d6e20053ff7fc425118810ba685f9f8d53ecbf
dmh1006 <dmh1006@alu.ubu.es>
2026-02-02 00:59:08 +0100

    problema arreglado y comenzamos con el main

--- utils/clases/PlanificadorTramos.py
+++ utils/clases/PlanificadorTramos.py
@@ -2,23 +2,12 @@ from utils.clases.ProblemaRuta import ProblemaRuta
 from utils.clases.ResultadoBusqueda import ResultadoBusqueda
 
 
-class PlanificadorPorTramos:
-    """
-    Planifica una ruta pasando por varios objetivos en orden.
-    Cada tramo se resuelve como un problema independiente.
-    """
-
+class PlanificadorTramos:
     def __init__(self, mapa, penalizaciones):
         self.mapa = mapa
         self.penalizaciones = penalizaciones
 
     def planificar(self, inicio, objetivos, algoritmo_busqueda):
-        """
-        inicio: (fila, columna)
-        objetivos: lista de estados [(f,c), (f,c), ...]
-        algoritmo_busqueda: función de búsqueda (anchura, voraz o A*)
-        """
-
         camino_total = []
         coste_total = 0
 
@@ -29,21 +18,13 @@ class PlanificadorPorTramos:
         actual = inicio
 
         for obj in objetivos:
-            problema = ProblemaRuta(
-                actual,
-                obj,
-                self.mapa,
-                self.penalizaciones
-            )
-
+            problema = ProblemaRuta(actual, obj, self.mapa, self.penalizaciones)
             resultado = algoritmo_busqueda(problema)
 
-            # Acumulamos métricas
             nodos_expandidos_total += resultado.get_nodos_expandidos()
             frontera_max_total = max(frontera_max_total, resultado.get_frontera_max())
             tiempo_total += resultado.get_tiempo_ms()
 
-            # Si no hay solución en algún tramo, se cancela todo
             if not resultado.hay_solucion():
                 return ResultadoBusqueda(
                     False,
@@ -53,18 +34,15 @@ class PlanificadorPorTramos:
                     tiempo_ms=tiempo_total
                 )
 
-            camino_tramo = resultado.get_camino()
-
-            # Concatenar caminos evitando repetir el punto inicial del tramo
+            tramo = resultado.get_camino()
             if not camino_total:
-                camino_total.extend(camino_tramo)
+                camino_total.extend(tramo)
             else:
-                camino_total.extend(camino_tramo[1:])
+                camino_total.extend(tramo[1:])
 
             coste_total += resultado.get_coste()
             actual = obj
 
-        # Nodo final "artificial" para encapsular el camino completo
         nodo_final = _NodoFinal(camino_total, coste_total)
 
         return ResultadoBusqueda(
@@ -77,9 +55,6 @@ class PlanificadorPorTramos:
 
 
 class _NodoFinal:
-    """
-    Nodo mínimo para representar un camino completo ya concatenado.
-    """
     def __init__(self, camino, coste):
         self._camino = camino
         self._coste = coste
